0076917eb28670be70ce9b799ee17ea2b3612795
harshcasper <hmishra@redhat.com>
2021-09-19 09:41:28 +0530

    FEAT: Add Calculation Demo example

---
+++ examples/calculation-demo/README.md
@@ -0,0 +1,10 @@
+# Calculation Demo
+
+The following code snippets demonstrate a simple use-case of using Python functions inside a Node app.
+
+To run:
+
+```sh
+metacall main.js
+```
+
---
+++ examples/calculation-demo/main.js
@@ -0,0 +1,9 @@
+const { sum, sub, mul, div } = require('./app.py')
+
+console.log(sum(3,4)); // Sum: 7
+
+console.log(sub(5,2)); // Subtraction: 3
+
+console.log(mul(4,6)); // Multiplication: 24
+
+console.log(div(5,6)); // Divison: 30
